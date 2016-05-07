36ff246bfd7229851ea54a8a1fe221f6466d991c
supperkozak <supperkozak@gmail.com>
2016-05-07 11:33:20 +0300

    Commit

---
+++ crud/src/main/java/com/crud/model/Book.java
@@ -0,0 +1,66 @@
+package com.crud.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "BOOKS")
+
+public class Book {
+	
+	@Id
+	@Column(name = "ID")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(name = "TITLE")
+	private String title;
+	
+	@Column(name = "AUTHOR")
+	private String author;
+	
+	@Column(name = "PRISE")
+	private int prise;
+	
+	@Column(name = "DESCRIPTION")
+	private String description;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public int getPrise() {
+		return prise;
+	}
+	public void setPrise(int prise) {
+		this.prise = prise;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	@Override
+	public String toString() {
+		return "Book [id=" + id + ", title=" + title + ", author=" + author + ", prise=" + prise + "]";
+	}
+	
+	 	
+	
+
+}
---
+++ crud/src/main/java/com/crud/service/BookServiceImpl.java
@@ -0,0 +1,48 @@
+package com.crud.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.crud.dao.BookDao;
+import com.crud.model.Book;
+
+@Service
+public class BookServiceImpl implements BookService{
+	private BookDao bookDao;
+	
+	public void setBookDao(BookDao bookDao) {
+		this.bookDao = bookDao;
+	}
+
+	@Transactional
+	public void addBook(Book book) {
+		this.bookDao.addBook(book);
+		
+	}
+
+	@Transactional
+	public void updateBook(Book book) {
+		this.bookDao.updateBook(book);
+		
+	}
+
+	@Transactional
+	public void removeBook(int id) {
+		this.bookDao.removeBook(id);
+		
+	}
+
+	@Transactional
+	public Book getBookById(int id) {
+		return this.bookDao.getBookById(id);
+	}
+
+	@Transactional
+	public List<Book> listBooks() {
+		return this.bookDao.listBooks();
+	}
+	 
+
+}
